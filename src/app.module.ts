87e2681ad140e6b3b6d889cb7e3bfd582cd8bf5f
Ricardo Cortes <ricardo.ac0702@gmail.com>
2024-04-18 20:08:12 -0600

    revision 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,6 +15,11 @@ import { ProduccionModule } from './Produccion/produccion.module';
     ConfigModule.forRoot(),
 
     TypeOrmModule.forRoot({
+      ssl: process.env.STAGE === 'prod',
+      extra: {
+        ssl:
+          process.env.STAGE === 'prod' ? { rejectUnauthorized: false } : null,
+      },
       type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
